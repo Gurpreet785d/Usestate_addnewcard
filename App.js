fa291a5174ae593fd6564ab4170f9a8a24e821e1
Gurpreet Singh <120656666+Gurpreet785d@users.noreply.github.com>
2023-04-06 14:51:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import In from './component/In'
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Add_input/> */}
+      <In/>
+      {/* <Example/> */}
+    </div>
+  )
+}
+
+export default App
